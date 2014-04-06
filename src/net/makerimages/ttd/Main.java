340e8c4ac71ccd64625b77d7a4135e3b2dfe9910
makerimages <makerimagesgames@gmail.com>
2014-04-06 14:52:57 +0300

    Getting started
    Added Main Menu state.

---
+++ src/net/makerimages/ttd/Main.java
@@ -0,0 +1,41 @@
+package net.makerimages.ttd;
+
+import net.makerimages.ttd.state.MainMenuState;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+/**
+ * Created by Makerimages on 6.04.2014.
+ */
+public class Main extends StateBasedGame
+{
+    public static final String VERSION="1.0a4 indev";
+    public Main(String title)
+    {
+        super(title);
+    }
+
+    @Override
+    public void initStatesList(GameContainer gameContainer) throws SlickException
+    {
+        //TODO: add States here!
+        addState(new MainMenuState(0));
+    }
+    public void create() throws SlickException
+    {
+        AppGameContainer appGameContainer=new AppGameContainer(this);
+        appGameContainer.setDisplayMode(800, 600, false);
+        appGameContainer.setVSync(true);
+        appGameContainer.start();
+
+    }
+
+    public static void main(String[] args) throws SlickException
+    {
+        Main main=new Main("Trapped 'til Doom v: "+VERSION);
+        main.create();
+    }
+
+}
---
+++ src/net/makerimages/ttd/state/MainMenuState.java
@@ -0,0 +1,41 @@
+package net.makerimages.ttd.state;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+/**
+ * Created by Makerimages on 6.04.2014.
+ */
+public class MainMenuState extends BasicGameState
+{
+    public int id;
+    public MainMenuState(int id)
+    {
+        this.id=id;
+    }
+    @Override
+    public int getID() {
+        return this.id;
+    }
+
+    @Override
+    public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException
+    {
+
+    }
+
+    @Override
+    public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics graphics) throws SlickException
+    {
+
+    }
+
+    @Override
+    public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int i) throws SlickException
+    {
+
+    }
+}
